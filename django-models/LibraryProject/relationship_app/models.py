04201430dc9906e85880e953e626877801edb549
Alhassan Abdullah <alhassanabdullah977@gmail.com>
2025-09-09 07:00:29 +0000

    Add django-models project with relationship_app and models

---
+++ django-models/LibraryProject/relationship_app/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+# Book model (Many Books belong to one Author)
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+
+    def __str__(self):
+        return self.title
+
+
+# Library model (Many Libraries can have Many Books)
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book, related_name="libraries")
+
+    def __str__(self):
+        return self.name
+
+
+# Librarian model (One Librarian per Library)
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name="librarian")
+
+    def __str__(self):
+        return self.name
+# Create your models here.
