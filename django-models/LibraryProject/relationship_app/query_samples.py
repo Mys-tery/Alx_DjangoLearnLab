cbcc0ea80b6e14c39398f4903b82c0ef0ae1152c
Alhassan Abdullah <alhassanabdullah977@gmail.com>
2025-09-09 07:18:45 +0000

    Add query functions for author, library, and librarian in query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,10 +1,16 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
-books_by_author = Book.objects.filter(author="Author Name")
+def get_books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
+    return Book.objects.filter(author=author)
 
 # List all books in a library
-books_in_library = Library.objects.get(name="Library Name").books.all()
+def get_books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return library.books.all()
 
 # Retrieve the librarian for a library
-librarian = Librarian.objects.get(library="Library Name")
+def get_librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return Librarian.objects.get(library=library)
