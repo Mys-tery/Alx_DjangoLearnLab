79fb47d4c1c8f59171b84c6abac6c2dd6fb7a2bd
Alhassan Abdullah <alhassanabdullah977@gmail.com>
2025-09-16 03:36:21 +0000

    Add custom permissions to Book model with secured add, edit, and delete views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,32 +1,38 @@
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login, logout
-from django.shortcuts import redirect
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required, permission_required
+from .models import Book
 
-# Register view
-def register_view(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)  # auto-login after registration
-            return redirect('list_books')
-    else:
-        form = UserCreationForm()
-    return render(request, 'relationship_app/register.html', {'form': form})
+# Add Book View
+@login_required
+@permission_required('relationship_app.can_add_book', raise_exception=True)
+def add_book(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        author = request.POST.get("author")
+        published_date = request.POST.get("published_date")
+        Book.objects.create(title=title, author=author, published_date=published_date)
+        return redirect("book_list")
+    return render(request, "add_book.html")
 
-# Login view
-def login_view(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('list_books')
-    else:
-        form = AuthenticationForm()
-    return render(request, 'relationship_app/login.html', {'form': form})
+# Edit Book View
+@login_required
+@permission_required('relationship_app.can_change_book', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == "POST":
+        book.title = request.POST.get("title")
+        book.author = request.POST.get("author")
+        book.published_date = request.POST.get("published_date")
+        book.save()
+        return redirect("book_list")
+    return render(request, "edit_book.html", {"book": book})
 
-# Logout view
-def logout_view(request):
-    logout(request)
-    return render(request, 'relationship_app/logout.html')
+# Delete Book View
+@login_required
+@permission_required('relationship_app.can_delete_book', raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == "POST":
+        book.delete()
+        return redirect("book_list")
+    return render(request, "delete_book.html", {"book": book})
