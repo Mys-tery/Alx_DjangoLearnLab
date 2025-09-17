e821c3ff5eeeb8f1093ea733329e520113c493f6
Alhassan Abdullah <alhassanabdullah977@gmail.com>
2025-09-17 08:11:14 +0000

    WIP: custom user model and cleanup

--- django-models/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("admin-dashboard/", views.admin_view, name="admin_view"),
-    path("librarian-dashboard/", views.librarian_view, name="librarian_view"),
-    path("member-dashboard/", views.member_view, name="member_view"),
-]
