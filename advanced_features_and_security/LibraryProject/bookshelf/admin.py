6ee62694fd73ba9d21757a52f069a242c55ea3d2
Alhassan Abdullah <alhassanabdullah977@gmail.com>
2025-09-20 00:12:30 +0000

    Add CustomUser admin registration in bookshelf/admin.py to match assignment requirements

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from users.models import CustomUser  # import from the users app
 
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  
-    search_fields = ('title', 'author')                     
-    list_filter = ('publication_year',)                     
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {"fields": ("date_of_birth", "profile_photo")}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {"fields": ("date_of_birth", "profile_photo")}),
+    )
 
-admin.site.register(Book, BookAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
