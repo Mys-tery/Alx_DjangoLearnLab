46d439f5deccca002737f42c31a24cc25edef6aa
Alhassan Abdullah <alhassanabdullah977@gmail.com>
2025-09-20 00:05:55 +0000

    Register CustomUser with CustomUserAdmin in admin interface

--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from users.admin import CustomUserAdmin  # noqa
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {"fields": ("date_of_birth", "profile_photo")}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {"fields": ("date_of_birth", "profile_photo")}),
+    )
 
-# Register your models here.
+admin.site.register(CustomUser, CustomUserAdmin)
