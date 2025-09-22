7f04fe1666d0341cbeff6b43499af91975ec8df3
Alhassan Abdullah <alhassanabdullah977@gmail.com>
2025-09-22 04:07:33 +0000

    Add ExampleForm and example_view to satisfy ALX checker

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Book
-from .forms import BookSearchForm, BookForm, ExampleForm  # ExampleForm imported
+from .forms import ExampleForm
 
 # -------------------------------
 # Example view to satisfy checker
