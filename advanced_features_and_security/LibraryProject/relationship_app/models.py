70dd37534cde2731f90a85980efd1ed72d47af72
Alhassan Abdullah <alhassanabdullah977@gmail.com>
2025-09-22 03:22:23 +0000

    Add CustomUser model with custom permissions and admin registration

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,9 +1,12 @@
 from django.db import models
-
+from django.conf import settings  
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
-    published_date = models.DateField()
+    published_date = models.DateField(null=True, blank=True)
+    isbn = models.CharField(max_length=13, unique=True)
+    added_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
 
     class Meta:
         permissions = [
