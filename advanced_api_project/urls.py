5d0ff4deea9f994d9ca9bdee27e261a68da7a413
Alhassan Abdullah <alhassanabdullah977@gmail.com>
2025-10-03 16:46:29 +0000

    Add Book views and API endpoints with DRF

--- advanced_api_project/urls.py
+++ advanced_api_project/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # ✅ API routes
+    path('api/', include('api.urls')),  # connects to your app
 ]
--- api/views.py
+++ api/views.py
@@ -1,11 +1,24 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
 
+# List all books & Create new book
 class BookListCreateView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return [permissions.IsAuthenticated()]
+        return [permissions.AllowAny()]
+
+
+# Retrieve, Update & Delete a single book
 class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    def get_permissions(self):
+        if self.request.method in ['PUT', 'PATCH', 'DELETE']:
+            return [permissions.IsAuthenticated()]
+        return [permissions.AllowAny()]
