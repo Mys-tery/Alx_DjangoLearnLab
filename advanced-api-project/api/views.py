4c1f4e25ab1403a3b1405af6147f2e3358436e91
Alhassan Abdullah <alhassanabdullah977@gmail.com>
2025-10-05 23:23:10 +0000

    Complete Book API with filtering, searching, ordering, and CRUD

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,6 +4,7 @@ from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 import django_filters
 from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
 
 class BookFilter(django_filters.FilterSet):
     author_name = django_filters.CharFilter(field_name='author__name', lookup_expr='icontains')
